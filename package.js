35ec00f1df85aff316351c78d28bd820012d0b9d
Anthony <Anthony123272@gmail.com>
2015-08-21 16:00:11 -0500

    Standard formatting & version package.js

--- package.js
+++ package.js
@@ -2,15 +2,18 @@
 /*global Package*/
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.0.3.1');
-	api.use('standard-app-packages');
-	api.use([	'accounts-password',
-				'random'
+	api.versionsFrom('1.1.0.3');
+
+	api.use([
+		'standard-app-packages',
+		'accounts-password',
+		'random'
 	]);
 
-	api.addFiles('export.js');
-
-	api.addFiles('auto-guest.js');
+	api.addFiles([
+		'export.js',
+		'auto-guest.js'
+	]);
 
 	api.export('AutoGuest');
 });
