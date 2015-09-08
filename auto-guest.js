76f50c9ed8dde28bd51be751daa74328ab79cfc5
Anthony <Anthony123272@gmail.com>
2015-09-08 11:58:04 -0500

    Validate unique username pre createUser +misc cleanup & prep

--- auto-guest.js
+++ auto-guest.js
@@ -46,6 +46,8 @@ AutoGuest.go = function(cb) {
 /*
  * Like Account.createUser but presumes we start with an existing guest user
  * * Same parameters & callback use
+ *
+ * TODO: Pass real error objects to callbacks
  */
 AutoGuest.createUser = function(options, callback) {
 	// AutoGuest: Instead of creating a new user, update guest user
@@ -60,16 +62,33 @@ AutoGuest.createUser = function(options, callback) {
 			verified: false
 		}];
 	}
-	// TODO: Return object with reason if username or email already taken
+	// Check if username taken
+	if (Meteor.users.findOne({ username: options.username })) {
+		return callback(new Meteor.Error('username-taken', 'Username already registered.'));
+	}
+	/*
+	// Check if email address taken
+	// TODO: Make this work
+	console.log(options.email);
+	if (options.email) {
+		//var Users = new Mongo.Collection('users');
+		var Users = Meteor.users;
+		// TODO: Figure out why isn't working, it's not finding users with the email address
+		var found = Users.find({ 'emails.address': options.email });
+		console.log(found.count());
+		//return callback(new Meteor.Error('email-taken', 'Email address already registered.'));
+	}
+	*/
+
 	Meteor.call('AutoGuestSetAccountPassword', options.password, function(error) {
 		if (error) {
-			callback(true);
+			return callback(true);
 		} else {
 			Meteor.users.update(userId, { $set: set }, {}, function(error) {
 				if (error) {
-					callback(true);
+					return callback(true);
 				} else {
-					callback(false);
+					return callback(false);
 				}
 			});
 		}
